648add89212b9bcc242ebeda32be185eb7088bfb
axwii <axwi0001@stud.kea.dk>
2024-12-10 13:38:40 +0100

    add buttons and accordion functionality to homepage; enhance layout and interactivity

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,46 @@ export default function Home() {
     <div>
       <Hero />
       <div className="flex space-x-40 justify-center mt-12">
-       <Button text="Program" />
-       <Button text="Billetter" />
-       <Button text="Frivillig" />
+        <Button text="Program" />
+        <Button text="Billetter" />
+        <Button text="Frivillig" />
       </div>
-      <h2 className="text-center text-8xl mt-12 font-Gambetta font-thin">Line-up</h2>
+      <h2 className="text-center text-8xl mt-12 font-Gambetta font-thin">
+        Line-up
+      </h2>
       <Accordion />
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" defaultChecked />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
     </div>
   );
 }
