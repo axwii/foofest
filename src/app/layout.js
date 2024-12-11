c5ef50fffe7211c2447beeb3a71af32118acadc8
axwii <axwi0001@stud.kea.dk>
2024-12-11 22:24:26 +0100

    add Header component to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const geistSans = localFont({
