b0552c6588003793d2e2468e77906a1874fd336d
Nikolaj <nikolajgottschalck@hotmail.com>
2024-12-11 22:37:55 +0100

    add HeroSingle component and integrate into lineup page; clean up BandCard

---
+++ src/app/lineup/[slug].js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import HeroSingle from "../components/HeroSingle";
+
+export default function Home() {
+  return (
+    <div>
+<HeroSingle />
+
+      </div>
+  );
+}
