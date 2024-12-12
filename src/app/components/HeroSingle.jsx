340aa4d15794243b8605a484cbaaae7245065ff7
Nikolaj <nikolajgottschalck@hotmail.com>
2024-12-12 22:06:12 +0100

    reduce minimum height of HeroSingle component for improved layout

--- src/app/components/HeroSingle.jsx
+++ src/app/components/HeroSingle.jsx
@@ -1,7 +1,7 @@
 const HeroSingle = ({ band = {} }) => {
   const { name, logo } = band;
   return (
-    <div className="hero min-h-72 flex items-center justify-center relative">
+    <div className="hero min-h-52 flex items-center justify-center relative">
       <div
         className="absolute inset-0"
         style={{
