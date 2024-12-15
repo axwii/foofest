ee6621ee0c560413192cf832bf4819715705e5ec
axwii <axwi0001@stud.kea.dk>
2024-12-15 23:41:04 +0100

    add API functions for available spots, reserving a spot, and fulfilling reservations

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -35,3 +35,35 @@ export async function getSchedule() {
   const data = await response.json();
   return data;
 }
+
+export async function getAvailableSpots() {
+  const response = await fetch(`${url}/available-spots`, {
+    method: "GET",
+    headers: headersList,
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+export async function PutReserveSpot(area, amount) {
+  const response = await fetch(`${url}/reserve-spot`, {
+    method: "PUT",
+    headers: headersList,
+    body: JSON.stringify({ area, amount }),
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+export async function PostFulfillReservation(reservationId) {
+  const response = await fetch(`${url}/fullfill-reservation`, {
+    method: "POST",
+    headers: headersList,
+    body: JSON.stringify({ id: reservationId }),
+  });
+
+  const data = await response.json();
+  return data;
+}
