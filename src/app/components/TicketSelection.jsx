1ea8f97e11e6c730a702493f15bbaadf67142952
axwii <axwi0001@stud.kea.dk>
2024-12-16 14:19:25 +0100

    implement form handling in TicketSelection component using react-hook-form for ticket selection

--- src/app/components/TicketSelection.jsx
+++ src/app/components/TicketSelection.jsx
@@ -1,14 +1,37 @@
 "use client";
 
 import { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-export default function TicketSelection( ) {
+export default function TicketSelection() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
 
-  
-    return (
-      <div>
-        <h2>Select Your Tickets</h2>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h2>Select Your Tickets</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="number"
+          placeholder="Regular"
+          {...register("Regular", { min: 0 })}
+          className="input input-bordered"
+        />
+        <input
+          type="number"
+          placeholder="VIP"
+          {...register("VIP", { min: 0 })}
+          className="input input-bordered"
+        />
+
+        <input type="submit" className="btn"/>
+      </form>
+    </div>
+  );
+}
