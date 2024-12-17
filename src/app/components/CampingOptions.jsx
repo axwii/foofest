fed4ec20e992b13522bda90269c8a85029e88692
axwii <axwi0001@stud.kea.dk>
2024-12-17 02:47:26 +0100

    add form handling and display available camping spots in CampingOptions component

--- src/app/components/CampingOptions.jsx
+++ src/app/components/CampingOptions.jsx
@@ -1,14 +1,37 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { getAvailableSpots } from "@/app/lib/api";
 
 export default function CampingOptions() {
-  const data = getAvailableSpots();
-  console.log(data);
+  const [spots, setSpots] = useState([]);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
+  useEffect(() => {
+    getAvailableSpots().then(setSpots);
+  }, []);
 
   return (
     <div>
       <h2>Camping Options</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {spots.map((spot) => (
+          <div key={spot.area}>
+            <label>
+              <input
+                {...register("Area")}
+                type="radio"
+                value={spot.area}
+                disabled={spot.available === 0}
+              />
+              {spot.area} ({spot.available} spots available)
+            </label>
+          </div>
+        ))}
+        <input type="submit" />
+      </form>
     </div>
   );
 }
