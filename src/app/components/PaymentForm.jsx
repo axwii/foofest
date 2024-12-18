f3f76c432985fd4c35cee22b9cec9951667ac1c5
axwii <axwi0001@stud.kea.dk>
2024-12-18 17:50:45 +0100

    feat: add PaymentCard component for credit card input handling

--- src/app/components/PaymentForm.jsx
+++ src/app/components/PaymentForm.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import PaymentCard from "../components/PaymentCard";
 
 export default function TicketSelection() {
   return (
     <div>
       <h2>Payment Information</h2>
+      <PaymentCard />
     </div>
   );
 }
