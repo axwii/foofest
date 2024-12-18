7fcdc37973dcb2c25deec2902a46ed5aa3a16648
Nikolaj <nikolajgottschalck@hotmail.com>
2024-12-18 16:13:42 +0100

    add schedule prop to BandList component and fetch schedule data in Home

--- src/app/components/BandList.jsx
+++ src/app/components/BandList.jsx
@@ -1,13 +1,13 @@
 import BandCard from "./BandCard";
-const BandList = ({ bands }) => {
+const BandList = ({ bands, schedule}) => {
   if (bands.length === 0) {
     return <p>Ingen bands fundet</p>;
   }
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10 gap-y-10 mx-[50px]">
-      {bands.map((band) => (
-        <BandCard  band={band} key={band.slug} />
+      {bands.map((band, schedule) => (
+        <BandCard  band={band} schedule={schedule} key={band.slug} />
       ))}
     </div>
   );
--- src/app/lineup/page.js
+++ src/app/lineup/page.js
@@ -1,13 +1,15 @@
 import { getBands } from "@/app/lib/api";
+import { getSchedule } from "@/app/lib/api";
 import BandList from "../components/BandList";
 
 export default async function Home() {
   const bands = await getBands();
+  const schedule = await getSchedule();
 //   console.log(bands);
 
   return (
     <div className="bg-black">
-      <BandList bands={bands} />
+      <BandList bands={bands} schedule={schedule} />
     </div>
   );
 }
