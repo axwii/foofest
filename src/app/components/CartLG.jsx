c2a7fc334f31ad040075e592ae7094a6fa0f28ce
Nikolaj <nikolajgottschalck@hotmail.com>
2024-12-19 00:31:34 +0100

    feat: enhance Cart components with improved styling and layout for better ticket display

--- src/app/components/CartLG.jsx
+++ src/app/components/CartLG.jsx
@@ -1,10 +1,29 @@
 
 const CartLG = ({ ticketData }) => {
   return (
-    <div className="lg:w-[250px] lg:h-[300px] bg-[#242424] p-4 rounded-lg shadow-lg fixed right-5 top-1/2 transform -translate-y-1/2 hidden lg:block">
-      <h1>Cart</h1>
-      <p>Regular Tickets: {ticketData.regular}</p>
-      <p>VIP Tickets: {ticketData.vip}</p>
+    <div className="lg:w-[250px] lg:min-h-[300px] bg-[#242424] p-4 rounded-3xl shadow-lg fixed right-5 top-1/2 transform -translate-y-1/2 hidden lg:block">
+      <div className="ml-2">
+      <h1 className="text-center text-3xl font-GermaniaOneRegular">Cart</h1>
+      <h2 className="font-bold text-xl  font-GermaniaOneRegular">Tickets</h2>
+      <div className=" mt-2 font-Gambetta flex justify-between">
+        <div>
+      <p>VIP Tickets:</p>
+      <p className="text-sm font-thin">1299 DKK</p>
+      </div>
+      <p className="justify-end mt-3">X {ticketData.vip}</p>
+      </div>
+      <hr className="my-4"  />
+      <h2 className="font-bold text-xl  font-GermaniaOneRegular">Camping</h2>
+      <p className="font-Gambetta">Alfheim</p>
+      <hr className="my-4" />
+      <div className="flex justify-between font-Gambetta">
+        <p className="text-lg">Subtotal:</p>
+        
+      <p className="justify-end text-lg">799DKK</p>
+      
+      </div>
+      
+      </div>
     </div>
   );
 };
--- src/app/components/CartSM.jsx
+++ src/app/components/CartSM.jsx
@@ -1,16 +1,33 @@
 const CartSM = ({ ticketData }) => {
     return (
       <>
-        {/* Open the modal using document.getElementById('my_modal_1').showModal() method */}
-        <button className="btn lg:hidden fixed right-5 top-1/2 transform -translate-y-1/2" onClick={() => document.getElementById('my_modal_1').showModal()}>Cart</button>
+        <button className="btn hover:bg-[#242424] lg:hidden fixed right-5 top-12 z-20 transform bg-[#242424] -translate-y-1/2" onClick={() => document.getElementById('my_modal_1').showModal()}>Cart</button>
         <dialog id="my_modal_1" className="modal lg:hidden">
           <div className="modal-box bg-[#242424] p-4 rounded-lg shadow-lg">
-          <h1>Cart</h1>
-      <p>Regular Tickets: {ticketData.regular}</p>
-      <p>VIP Tickets: {ticketData.vip}</p>
+          <div className="ml-2">
+      <h1 className="text-center text-3xl font-GermaniaOneRegular">Cart</h1>
+      <h2 className="font-bold text-xl  font-GermaniaOneRegular">Tickets</h2>
+      <div className=" mt-2 font-Gambetta flex justify-between">
+        <div>
+      <p>VIP Tickets:</p>
+      <p className="text-sm font-thin">1299 DKK</p>
+      </div>
+      <p className="justify-end mt-3">X {ticketData.vip}</p>
+      </div>
+      <hr className="my-4"  />
+      <h2 className="font-bold text-xl  font-GermaniaOneRegular">Camping</h2>
+      <p className="font-Gambetta">Alfheim</p>
+      <hr className="my-4" />
+      <div className="flex justify-between font-Gambetta">
+        <p className="text-lg">Subtotal:</p>
+        
+      <p className="justify-end text-lg">799DKK</p>
+      
+      </div>
+      
+      </div>
             <div className="modal-action">
               <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
                 <button className="btn">Close</button>
               </form>
             </div>
