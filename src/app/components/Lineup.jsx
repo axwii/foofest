c96ce9bc94104b83d14959a2f4f8ba4d9fe65355
Nikolaj <nikolajgottschalck@hotmail.com>
2024-12-18 12:44:50 +0100

    refactor Lineup component to enhance band display with links and improve layout

--- src/app/components/Lineup.jsx
+++ src/app/components/Lineup.jsx
@@ -1,15 +1,52 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 
 const Lineup = ({ bands }) => {
+ 
   if (bands.length === 0) {
     return <p>Ingen bands fundet</p>;
   }
-  const { name } = bands;
+
   return (
-    <div>
-      <p>{name}</p>
+    <div className="text-center m-auto gap-10 justify-center flex flex-wrap w-[75%] mt-10">
+      {bands.slice(0, 6).map((band, index) => (
+        <Link 
+          key={index} 
+          href={{
+            pathname: `/lineup/${band.slug}`,
+            query: { ...band }
+          }}
+          alt={`${band.name} logo`} 
+        >
+          <p className="text-5xl hover:text-red-500 hover:scale-105">{band.name}</p>
+        </Link>
+      ))}
+      {bands.slice(7, 17).map((band, index) => (
+                <Link 
+                key={index} 
+                href={{
+                  pathname: `/lineup/${band.slug}`,
+                  query: { ...band }
+                }}
+                alt={`${band.name} logo`} 
+              >
+                <p className="text-3xl hover:text-red-500 hover:scale-105">{band.name}</p>
+              </Link>
+      ))}
+      {bands.slice(18, 28).map((band, index) => (
+                <Link 
+                key={index} 
+                href={{
+                  pathname: `/lineup/${band.slug}`,
+                  query: { ...band }
+                }}
+                alt={`${band.name} logo`} 
+              >
+                <p className="text-xl hover:text-red-500 hover:scale-105">{band.name}</p>
+              </Link>
+      ))}
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -11,16 +11,15 @@ export default async function Home() {
     <div>
       <Hero />
       <div className="flex space-x-40 justify-center mt-12">
-        <Button text="Program" link="lineup"/>
-        <Button text="Tickets" link="ticket"/>
-        <Button text="Volunteer" link="volunteer"/>
+        <Button text="Program" link="lineup" />
+        <Button text="Tickets" link="ticket" />
+        <Button text="Volunteer" link="volunteer" />
       </div>
       <h2 className="text-center text-8xl mt-12 font-Gambetta font-thin">
         Line-up
       </h2>
-      <Lineup bands={bands} />
+      <Lineup bands={bands} key={bands.slug} />
       <Accordion />
-
-      </div>
+    </div>
   );
 }
